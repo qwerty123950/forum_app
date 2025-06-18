e1333d683600bc79c878cdcc2f2a67696196631f
qwerty123950 <ben.kattackan123@gmail.com>
2025-06-18 15:16:14 +0530

    Ready for Deployment

--- forum/settings.py
+++ forum/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6d7^wwqbt(kcmm*a9mhox2_l$cwg7a*x$j^_a0ydc(e=)6dbbg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'forum.urls'
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
