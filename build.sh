62f155eff4190303453613bc79efabc4670a2199
qwerty123950 <ben.kattackan123@gmail.com>
2025-06-19 10:02:50 +0530

    temp: run migrate during build

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-
-# install dependencies
 pip install -r requirements.txt
-
-# collect static files
-python manage.py collectstatic --noinput
-
-# apply migrations
 python manage.py migrate
+python manage.py collectstatic --noinput
